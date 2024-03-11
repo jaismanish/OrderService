43e8b38921c642ce8ed6831b022517acad136977
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-12 02:42:59 +0530

    added field in Order REsponse

--- src/main/java/OrderService/models/OrderResponse.java
+++ src/main/java/OrderService/models/OrderResponse.java
@@ -1,4 +1,22 @@
 package OrderService.models;
 
+import OrderService.entities.Item;
+import OrderService.enums.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponse {
+    private Integer orderId;
+    private Integer restaurantId;
+    private Double totalPrice;
+    private String customer;
+    private List<Item> items;
+    private OrderStatus orderStatus;
+    private Integer deliveryValetId;
 }
