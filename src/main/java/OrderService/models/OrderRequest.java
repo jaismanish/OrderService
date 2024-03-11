1693b3e03362f26877d453b49c0bc4dcbad4c6a9
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-12 02:41:08 +0530

    added fields in Order Request

--- src/main/java/OrderService/models/OrderRequest.java
+++ src/main/java/OrderService/models/OrderRequest.java
@@ -1,4 +1,15 @@
 package OrderService.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
+    private Integer restaurantId;
+    private List<String> items;
 }
