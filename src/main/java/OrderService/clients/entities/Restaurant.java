6c63323a93054029098ea4c5970276dd8055ae1c
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-12 03:44:30 +0530

    created client's entities

---
+++ src/main/java/OrderService/clients/entities/Restaurant.java
@@ -0,0 +1,17 @@
+package OrderService.clients.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Restaurant {
+    private Integer restaurantId;
+    private String restaurantName;
+    private String address;
+    private Menu menu;
+}
