7d7e0e5b09aa6c6c9a625310cc592bbef0ff076a
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-03-13 13:36:23 +0530

    Added tests for Order service

--- src/test/java/OrderService/services/OrderServiceTest.java
+++ src/test/java/OrderService/services/OrderServiceTest.java
@@ -5,6 +5,7 @@ import OrderService.entities.Item;
 import OrderService.entities.Order;
 import OrderService.enums.OrderStatus;
 import OrderService.exception.NoDeliveryValetFoundException;
+import OrderService.exception.OrderNotFoundException;
 import OrderService.exception.UserNotRegisteredException;
 import OrderService.models.OrderRequest;
 import OrderService.models.OrderResponse;
@@ -53,14 +54,14 @@ class OrderServiceTest {
     void testCreateOrderSuccessfully() throws NoDeliveryValetFoundException {
         Customer customer = new Customer(1, "test", "test", "test", new ArrayList<>());
         List<Item> items = new ArrayList<>(List.of(new Item(1,1, "wings", 180.0), new Item(2,2, "rings", 180.0)));
-        Order order = new Order(1,  customer,1,OrderStatus.ACCEPTED,360.0,  items,1);
+        Order order = new Order(1,  customer,1,OrderStatus.ACCEPTED,400.0,  items,1);
         OrderRequest request = new OrderRequest(1, List.of("wings", "rings"));
         when(customerRepository.findByUsername("test")).thenReturn(Optional.of(customer));
         when(authentication.getName()).thenReturn("test");
         when(securityContext.getAuthentication()).thenReturn(authentication);
         SecurityContextHolder.setContext(securityContext);
         when(ordersRepository.save(any())).thenReturn(order);
-        OrderResponse expected = new OrderResponse(1,1,360.0,"test", items,OrderStatus.ACCEPTED,1);
+        OrderResponse expected = new OrderResponse(1,1,400.0,"test", items,OrderStatus.ACCEPTED,1);
 
         OrderResponse actual = ordersService.create("test", request);
 
@@ -68,5 +69,38 @@ class OrderServiceTest {
         verify(customerRepository, times(1)).findByUsername("test");
         verify(ordersRepository, times(1)).save(any());
     }
+
+    @Test
+    void testFetchOrderSuccessfully() throws UserNotRegisteredException, OrderNotFoundException {
+        List<Item> items = new ArrayList<>(List.of(new Item(1,1, "wings", 180.0), new Item(2,2, "rings", 180.0)));
+        Customer customer = new Customer(1, "test", "test", "test", new ArrayList<>());
+        Order order = new Order(1,  customer,1,OrderStatus.ACCEPTED,400.0,  items,1);
+        customer.getOrders().add(order);
+        when(customerRepository.findByUsername("test")).thenReturn(Optional.of(customer));
+        when(authentication.getName()).thenReturn("test");
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(ordersRepository.findById(1)).thenReturn(Optional.of(order));
+        OrderResponse expected = new OrderResponse(1,1,400.0,"test", items,OrderStatus.ACCEPTED,1);
+
+        OrderResponse actual = ordersService.fetch("test", 1);
+
+        assertEquals(expected,actual);
+        verify(customerRepository, times(1)).findByUsername("test");
+        verify(ordersRepository, times(1)).findById(1);
+    }
+
+    @Test
+    void testFetchOrder_ShouldExpectOrderNotFoundException() throws UserNotRegisteredException {
+        Customer customer = new Customer(1, "test", "test", "test", new ArrayList<>());
+        when(customerRepository.findByUsername("test")).thenReturn(Optional.of(customer));
+        when(authentication.getName()).thenReturn("test");
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(ordersRepository.findById(1)).thenReturn(Optional.empty());
+
+        assertThrows(OrderNotFoundException.class,()-> ordersService.fetch("test", 1));
+    }
+
 
 }
